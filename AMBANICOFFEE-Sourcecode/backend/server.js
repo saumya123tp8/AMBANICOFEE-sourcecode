5e96f6765adda2689972f9a1a070654dc8d51fbf
saumya123tp8 <95959848+saumya123tp8@users.noreply.github.com>
2024-02-03 15:56:14 +0530

    Add files via upload

---
+++ AMBANICOFFEE-Sourcecode/backend/server.js
@@ -0,0 +1,47 @@
+const app = require("./app");
+const express=require("express")
+const cors=require("cors")
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+// const dotenv=require("dotenv")
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+//config new
+// dotenv.config({ path: "backend/config/config.env" })
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+//   });
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
